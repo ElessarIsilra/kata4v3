88c05399dd3d7223773e369d88adb9369fae41be
usuario <usuario@10.22.143.15>
2015-10-27 10:03:22 +0000

    version con tiempo y buffer

--- src/kata4v3/Kata4v3.java
+++ src/kata4v3/Kata4v3.java
@@ -9,20 +9,27 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 
 public class Kata4v3 {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        String from = "C:\\Users\\usuario\\Desktop\\nuevo.txt";
-        String to = "C:\\Users\\usuario\\Desktop\\nuevo1.txt";
+        String from = "C:\\Users\\usuario\\Desktop\\T2 Abstraccion y polimorfismo.pdf";
+        String to = "C:\\Users\\usuario\\Desktop\\T2 Abstraccion y polimorfismo2.pdf";
+        Date date = new Date();
+        float first= date.getTime();
+        byte[] buffer = new byte[1024];
         InputStream input = new BufferedInputStream(new FileInputStream(from));
         OutputStream output = new BufferedOutputStream(new FileOutputStream(to));
         
         while(true){
-            int read = input.read();
+            int read = input.read(buffer);
             if(read < 0)break;
-            output.write(read);
+            output.write(buffer);
         }
+        Date date2 = new Date();
+        float second= date2.getTime();
+        System.out.println(second-first);
         input.close();
         output.flush();
         output.close();
